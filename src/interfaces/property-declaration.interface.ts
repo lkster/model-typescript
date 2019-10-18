7ea636381c328affdca27beadfd5bb344b31f189
Dominik Mielcarek <dominik.mielcarek@juliusworks.com>
2019-10-18 04:11:32 +0200

    Removed model type from PropertyDeclarationInterface

--- src/interfaces/property-declaration.interface.ts
+++ src/interfaces/property-declaration.interface.ts
@@ -1,10 +1,9 @@
 import { TPropertyKey } from '../types/property-key.type';
 import { PropertyTypeEnum } from '../enums/property-type.enum';
-import { ImmutableModel } from '../immutable-model';
 
 
 export interface IPropertyDeclaration {
     key: TPropertyKey;
     type: PropertyTypeEnum;
-    model?: typeof ImmutableModel;
+    model?: any;
 }
