00c83ccb830619a47616b35a48b65525931dcb8c
Dominik Mielcarek <dominik.mielcarek@juliusworks.com>
2019-10-18 03:04:01 +0200

    Exposed ObjectUtils in index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { Prop } from './decorators/prop.decorator';
 import { ImmutableModel } from './immutable-model';
+import { ObjectUtils } from './utils/object.utils';
 
 export {
     Prop,
-    ImmutableModel
+    ImmutableModel,
+    ObjectUtils,
 }
